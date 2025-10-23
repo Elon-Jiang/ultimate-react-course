e1af3242a2b1715c9b71fb63dc41f939a9482f71
姜泽栋 <i-jiangzedong@shuwen.com>
2025-10-23 12:02:25 +0800

    feat(manual-ssr): 添加手动服务端渲染示例项目
    添加包含React服务端渲染的示例项目，包括服务器端代码、客户端代码和基础配置
    添加.gitignore文件忽略node_modules目录

---
+++ 20-manual-ssr/starter/sever.js
@@ -0,0 +1,92 @@
+const { readFileSync } = require('fs');
+const {createServer}=require('http')
+const {parse} = require('url')
+const {renderToString} = require('react-dom/server')
+const React = require('react')
+
+const pizzas = [
+  {
+    name: "Focaccia",
+    price: 6,
+  },
+  {
+    name: "Pizza Margherita",
+    price: 10,
+  },
+  {
+    name: "Pizza Spinaci",
+    price: 12,
+  },
+  {
+    name: "Pizza Funghi",
+    price: 12,
+  },
+  {
+    name: "Pizza Prosciutto",
+    price: 15,
+  },
+];
+
+function Home() {
+  return (
+    <div>
+      <h1>🍕 Fast React Pizza Co.</h1>
+      <p>This page has been rendered with React on the server 🤯</p>
+
+      <h2>Menu</h2>
+      <ul>
+        {pizzas.map((pizza) => (
+          <MenuItem pizza={pizza} key={pizza.name} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Counter() {
+  const [count, setCount] = React.useState(0);
+  return (
+    <div>
+      <button onClick={() => setCount((c) => c + 1)}>+1</button>
+      <span>{count}</span>
+    </div>
+  );
+}
+
+function MenuItem({ pizza }) {
+  return (
+    <li>
+      <h4>
+        {pizza.name} (${pizza.price})
+      </h4>
+      <Counter />
+    </li>
+  );
+}
+
+
+const htmlTemplate = readFileSync(`${__dirname}/index.html`,'utf-8')
+
+const sever = createServer((req,res)=>{
+    const pathName = parse(req.url,true).pathname;
+
+    if(pathName==='/'){
+        const renderedReact= renderToString(<Home/>)
+        const html = htmlTemplate.replace('%%%CONTENT%%%',renderedReact)
+        res.writeHead(200,{'Content-type':'text/html'})
+        res.end(html)
+    }else if(pathName==='/api'){
+        res.end('hello api')
+    }else if(pathName==='/client.js'){
+        const clientJs = readFileSync(`${__dirname}/client.js`,'utf-8')
+        res.writeHead(200,{'Content-type':'application/javascript'})
+        res.end(clientJs)
+    }else{
+        res.end('404 not found')
+    }
+    
+})
+
+sever.listen(3000,()=>{
+    console.log('server is running on port 3000')
+})
