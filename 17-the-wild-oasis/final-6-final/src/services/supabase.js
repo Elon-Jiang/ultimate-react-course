4501240343d4ab36d5825afe5bac070a05919975
姜泽栋 <i-jiangzedong@shuwen.com>
2025-10-23 12:02:03 +0800

    chore: 更新supabase服务的URL和密钥

--- 17-the-wild-oasis/final-6-final/src/services/supabase.js
+++ 17-the-wild-oasis/final-6-final/src/services/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = "https://dclaevazetcjjkrzczpc.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRjbGFldmF6ZXRjamprcnpjenBjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMyOTIzNDQsImV4cCI6MTk5ODg2ODM0NH0.LGg0M-taoHgKtxCzr9owrb09epnPaO_Yfz6xVE54sIY";
+export const supabaseUrl = "https://cedggpxdtvenohkmsgkh.supabase.co";
+const supabaseKey ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNlZGdncHhkdHZlbm9oa21zZ2toIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTcxNTk5MzMsImV4cCI6MjA3MjczNTkzM30.sWipxVkZqD1Ebl7_raBOXLbLHGjW36t5T_uuIupE-dI"
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
